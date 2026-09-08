8d06de850bce5767d24967bedb945bbfcc35aa4a
Thảo My Hoàng <26730049@ms.uit.edu.vn>
2026-09-09 00:12:26 +0700

    Update Snake.cpp

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -24,6 +24,22 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
+void drawBorder() {
+    for (int x = BOARD_LEFT; x <= BOARD_RIGHT; ++x) {
+        gotoxy(x, BOARD_TOP);
+        cout << '#';
+        gotoxy(x, BOARD_BOTTOM);
+        cout << '#';
+    }
+
+    for (int y = BOARD_TOP; y <= BOARD_BOTTOM; ++y) {
+        gotoxy(BOARD_LEFT, y);
+        cout << '#';
+        gotoxy(BOARD_RIGHT, y);
+        cout << '#';
+    }
+}
+
 class Snake {
 private:
     vector<Point> body;
@@ -92,6 +108,7 @@ int main() {
         }
 
         system("cls");
+        drawBorder();
         snake.draw();
         snake.move(direction);
         Sleep(150);
